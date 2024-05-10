7210d00d3d68c837609f4a32a4513adbe2ce1ec6
Hotboy2003 <alex.obyn@yandex.ru>
2024-05-10 11:49:15 +0200

    ничего не работает (годот для дебилов)

--- Mobs/skeleton.gd
+++ Mobs/skeleton.gd
@@ -3,40 +3,44 @@ extends CharacterBody2D
 var chase = false
 var speed = 100
 @onready var anim = $AnimatedSprite2D
+var alive = true
 
 func _physics_process(delta):
 	var player = $"../../Player"
 	var player_distance = position.distance_to(player.position)
 	
-	if player_distance < 500:
-		chase = true
-		anim.play("Run")
-	else:
-		chase = false
-		anim.play("idle")
+	if alive:
+		if player_distance < 500:
+			chase = true
+			anim.play("Run")
+		else:
+			chase = false
+			anim.play("idle")
+			
+		var direction = (player.position - position).normalized()
 		
-	var direction = (player.position - position).normalized()
+		if chase:
+			velocity.x = direction.x * speed
+			velocity.y = direction.y * speed
+		else:
+			velocity.x = 0
+			velocity.y = 0
+			
+		if direction.x < 0:
+			$AnimatedSprite2D.flip_h = true
+		else:
+			$AnimatedSprite2D.flip_h = false
 	
-	if chase:
-		velocity.x = direction.x * speed
-		velocity.y = direction.y * speed
-	else:
-		velocity.x = 0
-		velocity.y = 0
+		move_and_slide()
+
+
+func _on_death_body_entered(body):
+	if body.name == "Player":
+		death()
 		
-	if direction.x < 0:
-		$AnimatedSprite2D.flip_h = true
-	else:
-		$AnimatedSprite2D.flip_h = false
-	
-	move_and_slide()
-
-
-func _on_detector_body_entered(body):
-	if body.name == "Player":
-		chase = true
-
-
-func _on_detector_body_exited(body):
-	if body.name == "Player":
-		chase = false
+func death():
+	alive = false
+	anim.play("Death")
+	await anim.animation_finished
+	queue_free()
+		
